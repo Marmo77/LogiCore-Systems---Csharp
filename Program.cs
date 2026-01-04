17274207454e730bea95a6e9e0da12f4e967b74c
Marmo77 <matyskamela@gmail.com>
2026-01-05 00:32:03 +0100

    Magazyn - podstawa

--- Program.cs
+++ Program.cs
@@ -1,13 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-
 class Magazyn
 {
-    public Magazyn()
+    // Podstawowe dane
+    int id;
+    string localization;
+    string name;
+    
+    // Wysyłki
+    int prodDailyLimit;
+    int actualProd;
+    
+    // ograniczenia
+    //X
+
+    public Magazyn(string name,  string localization, int prodLimit)
     {
-        Console.WriteLine("Witamy w Magazynie!");
+        Random randID = new Random();
+        this.id = randID.Next();
+        this.localization = localization;
+        this.name = name;
+        this.prodDailyLimit = prodLimit;
+
+    }
+
+    public void DaneFirmy()
+    {
+        Console.WriteLine($"----- DANE FIRMY {name.ToUpper()} -----");
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Lokalizacaja: {localization}");
+        Console.WriteLine($"Limit Dzienny Wysyłek: {prodDailyLimit}");
+        Console.WriteLine($"Aktualna Liczba Wysyłek: {actualProd}");
     }
 }
+
 
 
 class Program
@@ -16,6 +42,7 @@ class Program
     {
         Console.WriteLine("HELLO!");
 
-        Magazyn mag1 = new Magazyn();
+        Magazyn mag1 = new Magazyn("Kasztan Export", "Berlin", 24);
+        mag1.DaneFirmy();
     }
 }
