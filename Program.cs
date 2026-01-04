321f9e9e2817eacf74cd9b4c8a2ad1602ecf1be9
Marmo77 <matyskamela@gmail.com>
2026-01-05 00:44:18 +0100

    podstawowy produkt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+
+// MAGAZYN
 class Magazyn
 {
     // Podstawowe dane
@@ -32,9 +34,51 @@ class Magazyn
         Console.WriteLine($"Limit Dzienny Wysyłek: {prodDailyLimit}");
         Console.WriteLine($"Aktualna Liczba Wysyłek: {actualProd}");
     }
+
+    public bool CzyMozeWyslac()
+    {
+        if (actualProd > prodDailyLimit)
+        {
+            return true;
+        }
+        else
+        { 
+           return false; 
+        }
+    }
 }
 
+// PRODUKT
+enum ProduktTyp
+{
+    Standard,
+    Kruchy,
+    Niebezpieczny,
+    Gabarytowy
+}
 
+class Produkt
+{
+    public int id;
+    public string Name { get; set; }
+    public float Weight { get; set; }
+    public int Size { get; set; }
+    public ProduktTyp Type { get; set; }
+
+    public Produkt(int id, string name, float weight, int size, ProduktTyp type)
+    {
+        this.id = id;
+        Name = name;
+        Weight = weight;
+        Size = size;
+        Type = type;
+    }
+
+    public void PokazProdukt()
+    {
+        Console.WriteLine($"Produkt {id}: {Name}, Typ: {Type}, Waga: {Weight}, Rozmiar: {Size}");
+    }
+}
 
 class Program
 {
@@ -44,5 +88,10 @@ class Program
 
         Magazyn mag1 = new Magazyn("Kasztan Export", "Berlin", 24);
         mag1.DaneFirmy();
+
+        // Przykład tworzenia produktu z dozwolonym typem
+        Produkt p1 = new Produkt(1, "Szklanka", 0.5f, 1, ProduktTyp.Kruchy);
+        p1.PokazProdukt();
+
     }
 }
