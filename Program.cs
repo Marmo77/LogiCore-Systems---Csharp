299987fc6810c37371735dc268a206724be21811
Marmo77 <matyskamela@gmail.com>
2026-01-05 00:12:49 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Magazyn
+{
+    public Magazyn()
+    {
+        Console.WriteLine("Witamy w Magazynie!");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HELLO!");
+
+        Magazyn mag1 = new Magazyn();
+    }
+}
